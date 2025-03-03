19f77427cd61f369d7bcb6e5e4270c165a70f528
thisisbatra <batranitin118@gmail.com>
2025-03-03 18:02:14 +0530

    Initail commit for Retail-Mitr

---
+++ src/main/java/com/erp/retail_mitr/RetailMitrApplication.java
@@ -0,0 +1,17 @@
+package com.erp.retail_mitr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RetailMitrApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(RetailMitrApplication.class, args);
+		System.out.println("*****");
+		System.out.println("==========Application Is Running==========");
+		System.out.println("*****");
+	}
+
+}
---
+++ src/test/java/com/erp/retail_mitr/RetailMitrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.erp.retail_mitr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RetailMitrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
